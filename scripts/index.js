77161726c743f3151b2a64d8bd62ed846820aa99
ElizavetaSavchenkova <LizaRogers@yandex.ru>
2022-02-20 22:59:39 +0400

    fix: Исправления после 1-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,63 +1,35 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
-const popupElementopen = document.querySelector('.profile__edit-button');
-console.log(popupCloseButtonElement);
-console.log(popupElementopen);
-
-let formElement = document.querySelector('.popup__info');
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');
-let formName = document.querySelector('.popup__form-name');
-let formDescription = document.querySelector('.popup__form-text');
-let buttonSubmit = document.querySelector('.popup__button-submit');
-let buttonLike = document.querySelector('.card__likes-button');
-let buttonLikeActive = document.querySelector('.button_active');
+const popupElementOpen = document.querySelector('.profile__edit-button');
+const formElement = document.querySelector('.popup__info');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+const titleInput = document.querySelector('.popup__form-input_name_title');
+const subtitleInput = document.querySelector('.popup__form-input_name_subtitle');
+const buttonSubmit = document.querySelector('.popup__button-submit');
+const buttonLike = document.querySelector('.card__likes-button');
+const buttonLikeActive = document.querySelector('.button_active');
 
 
-const openPopUp = function (){
+const openPopup = function (){
   popupElement.classList.add('popup_is-opened');
+  titleInput.value = profileName.textContent;
+  subtitleInput.value = profileDescription.textContent;
 };
 
-const closePopUp = function(){
+const closePopup = function(){
   popupElement.classList.remove('popup_is-opened');
 };
 
-
-formName.value = profileName.textContent;
-formDescription.value = profileDescription.textContent;
-
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
-    profileName.textContent = formName.value;
-    profileDescription.textContent = formDescription.value;
+  profileName.textContent = titleInput.value;
+  profileDescription.textContent = subtitleInput.value;
+  closePopup();
 };
 
+popupElementOpen.addEventListener('click', openPopup);
 
-
-const addLikes = function () {
-  buttonLike.classList.add('button_active');
-};
-
-
-
-const closePopUpByClickOnOverlay = function (event){
-  console.log(event.target, event.currentTarget);
-  if (event.target !== event.currentTarget){
-    return;
-  }
-    closePopUp();
-};
-
-
-popupElementopen.addEventListener('click', openPopUp);
-
-popupCloseButtonElement.addEventListener('click', closePopUp);
+popupCloseButtonElement.addEventListener('click', closePopup);
 
 formElement.addEventListener('submit', formSubmitHandler);
-
-buttonSubmit.addEventListener('click', closePopUp);
-
-buttonLike.addEventListener('click', addLikes);
-
-popupElement.addEventListener('click', closePopUpByClickOnOverlay);
