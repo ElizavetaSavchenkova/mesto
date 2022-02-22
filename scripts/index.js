6d1b3e9f4ab5a5cd679ead50beb05285a1afbc67
ElizavetaSavchenkova <LizaRogers@yandex.ru>
2022-02-22 15:48:02 +0400

    fix: Исправления после 2-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -4,15 +4,13 @@ const popupElementOpen = document.querySelector('.profile__edit-button');
 const formElement = document.querySelector('.popup__info');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
-const titleInput = document.querySelector('.popup__form-input_name_title');
-const subtitleInput = document.querySelector('.popup__form-input_name_subtitle');
-const buttonSubmit = document.querySelector('.popup__button-submit');
-
+const titleInput = formElement.querySelector('.popup__form-input_name_title');
+const subtitleInput = formElement.querySelector('.popup__form-input_name_subtitle');
 
 const openPopup = function () {
-  popupElement.classList.add('popup_is-opened');
   titleInput.value = profileName.textContent;
   subtitleInput.value = profileDescription.textContent;
+  popupElement.classList.add('popup_is-opened');
 };
 
 const closePopup = function () {
